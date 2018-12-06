2594dd82753713670ad86b8e6c12628863e0d72e
VisualLinux <david_horvath67@yahoo.com>
2018-12-06 18:32:31 +0100

    Initial commit

---
+++ CalculateDerivative/Derivative.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using CalculateDerivative.Operations;
+
+namespace CalculateDerivative
+{
+    public static class Derivative
+    {
+        public static MathExpression CalculateDerivative(MathExpression exp, string variableName) =>
+            exp.Root.Derivative(variableName);
+    }
+}
